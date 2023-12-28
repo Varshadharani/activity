b93a1ec330e1d24556c77c9cf1f53c890aa2b448
Varshadharani <varshadharani43@gmail.com>
2023-12-28 16:07:32 +0530

    C3 Button

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from './Components/Button';
+import SearchBar from './Components/SearchBar';
+import Feedback from './Components/Givefeedback';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button />
+        <SearchBar />
+        <Feedback />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
